18605adbb1d193ecbe536b3e2c4de6b850cff1cb
Suhail TH <suhailth17756@gmail.com>
2024-08-21 09:15:46 +0530

    category componet added

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import Navbar from "./components/Navbar";
 import Hero from "./components/Hero";
 import HeadlineCards from "./components/HeadlineCards";
 import Food from "./components/Food";
+import Category from "./components/Category";
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
       <Hero></Hero>
       <HeadlineCards/>
       <Food/>
+      <Category></Category>
     </div>
   );
 }
